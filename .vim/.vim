d8edf4cf7a4756e9935082dfcc9c8882c6d1b94f
riffrain <raindropops@gmail.com>
2021-02-20 19:59:27 +0900

    update

--- .vim/.vim
+++
@@ -1,1 +0,0 @@
-/home/tanaka/dotfiles/.vim
--- setup.sh
+++ setup.sh
@@ -10,7 +10,9 @@ ln -s $path'/.agignore' ~/.agignore
 # }}}
 
 # vim {{{
-ln -s $path'/.vim' ~/.vim
+if [ -f ~/.vimrc ]; then
+  mv ~/.vimrc ~/.vimrc.orig
+fi
 ln -s $path'/.vimrc' ~/.vimrc
 if [ ! -d "~/.cache/dein" ]; then
   mkdir -p ~/.cache/dein
@@ -46,12 +48,10 @@ if [ ! -f ~/.gitconfig ]; then
   touch ~/.gitconfig
 fi
 echo "[include] path = "$path"/git/git-alias" >> ~/.gitconfig
-
 if [ -f ~/.git-prompt.sh ]; then
   mv ~/.git-prompt.sh ~/.git-prompt.sh.orig
 fi
 ln -s $path'/git/git-prompt.sh' ~/.git-prompt.sh
-
 if [ -f ~/.git-completion.bash ]; then
   mv ~/.git-completion.bash ~/.git-completion.bash.orig
 fi
