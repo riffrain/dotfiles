51bb44a47871ac9755884d11d29eedd7ad114d49
riffrain <raindropops@gmail.com>
2022-01-30 01:20:12 +0900

    add nvim

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,195 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+-- NvimTree
+vim.cmd([[
+  let g:nvim_tree_disable_window_picker = 0
+  let g:nvim_tree_window_picker_chars = "sdfghjkl"
+  nn <Leader>e <cmd>NvimTreeToggle<CR>
+]])
+
+-- CtrlP
+vim.cmd([[
+  if executable('ag')
+    let g:ctrlp_use_caching=0
+    let g:ctrlp_user_command='ag %s -i -s --nocolor --nogroup -g ""'
+  en
+  let g:ctrlp_show_hidden = 1
+  nn <Leader>f <cmd>CtrlP<CR>
+  nn <Leader>l <cmd>CtrlPLine<CR>
+  nn <Leader>b <cmd>CtrlPBuffer<CR>
+]])
+
+-- colorscheme
+vim.cmd([[
+  let g:vscode_style = "dark"
+  let g:vscode_transparent = 1
+  let g:vscode_italic_comment = 1
+  colorscheme vscode
+]])
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use 'editorconfig/editorconfig-vim'
+
+  use 'cohama/lexima.vim'
+
+  use 'tpope/vim-surround'
+
+  use 'wellle/targets.vim'
+
+  use 'easymotion/vim-easymotion'
+
+  use 'ctrlpvim/ctrlp.vim'
+
+  use 'Mofiqul/vscode.nvim'
+
+  use 'neovim/nvim-lspconfig'
+  use {
+    'hrsh7th/cmp-nvim-lsp',
+    requires = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
+    },
+  }
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function()
+      require'nvim-tree'.setup {
+        disable_netrw       = true,
+        hijack_netrw        = true,
+        open_on_setup       = false,
+        ignore_ft_on_setup  = {},
+        auto_close          = false,
+        open_on_tab         = false,
+        hijack_cursor       = false,
+        update_cwd          = false,
+        update_to_buf_dir   = {
+          enable = true,
+          auto_open = true,
+        },
+        diagnostics = {
+          enable = false,
+          icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+          }
+        },
+        update_focused_file = {
+          enable      = false,
+          update_cwd  = false,
+          ignore_list = {}
+        },
+        system_open = {
+          cmd  = nil,
+          args = {}
+        },
+        filters = {
+          dotfiles = false,
+          custom = {}
+        },
+        git = {
+          enable = true,
+          ignore = true,
+          timeout = 500,
+        },
+        view = {
+          width = 30,
+          height = 30,
+          hide_root_folder = false,
+          side = 'left',
+          auto_resize = false,
+          mappings = {
+            custom_only = true,
+            list = {
+              { key = {'<CR>', 'l'}, action = 'edit' },
+              { key = '<C-v>', action = 'vsplit' },
+              { key = '<C-x>', action = 'split' },
+              { key = '<C-t>', action = 'tabnew' },
+              { key = '<', action = 'prev_sibling' },
+              { key = '>', action = 'next_sibling' },
+              { key = 'P', action = 'parent_node' },
+              { key = 'h', action = 'close_node' },
+              { key = 'K', action = 'first_sibling' },
+              { key = 'J', action = 'last_sibling' },
+              { key = 'I', action = 'toggle_ignored' },
+              { key = '.', action = 'toggle_dotfiles' },
+              { key = 'R', action = 'refresh' },
+              { key = 'ma', action = 'create' },
+              { key = 'md', action = 'remove' },
+              { key = 'mm', action = 'rename' },
+              { key = 'mx', action = 'cut' },
+              { key = 'mp', action = 'paste' },
+              { key = 'y', action = 'copy_name' },
+              { key = 'Y', action = 'copy_path' },
+              { key = 'gy', action = 'copy_absolute_path' },
+              { key = 'u', action = 'dir_up' },
+              { key = 'q', action = 'close' },
+              { key = '?', action = 'toggle_help' },
+            },
+          },
+          number = false,
+          relativenumber = false,
+          signcolumn = "yes"
+        },
+        trash = {
+          cmd = nil,
+          require_confirm = true
+        }
+      }
+    end
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function ()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {},
+          always_divide_middle = true,
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        extensions = {}
+      }
+    end
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,91 @@
+vim.cmd([[
+se encoding=utf-8
+
+let g:mapleader = "\<Space>"
+
+if &compatible
+  se nocompatible
+en
+
+aug MyAutoCmd
+  au!
+aug END
+
+let s:cache_home = '~/.cache'
+
+filetype plugin indent on
+syntax enable
+
+" Settings {{{
+se number
+se laststatus=2
+se showcmd
+se ruler
+se wildmenu
+se showmatch
+se matchtime=1
+se list
+se listchars=tab:¦\ ,trail:･,extends:>,precedes:<
+se backspace=indent,eol,start
+se title
+se whichwrap=b,s,[,],<,>
+se nocursorline
+au InsertLeave * se cursorline!
+se fenc=utf-8
+se nobackup
+se noswapfile
+se visualbell
+se wildmode=list:longest
+se nobackup
+se noswapfile
+se tabstop=2
+se shiftwidth=2
+se expandtab
+se softtabstop=0
+se showtabline=2
+se t_Co=256
+se mouse=
+se incsearch
+se hlsearch
+let &t_SI = "\<Esc>]50;CursorShape=1\x7"
+let &t_EI = "\<Esc>]50;CursorShape=0\x7"
+if has("termguicolors")
+  se termguicolors
+en
+se noshowmode
+se background=dark
+" }}}
+
+
+" Unlimited undo {{{
+if has('persistent_undo')
+    let s:undo_dir = expand(s:cache_home . '/vimundo')
+    if !isdirectory(s:undo_dir)
+      cal system('mkdir -p ' . shellescape(s:undo_dir))
+    en
+    se undodir=~/.cache/vimundo
+    se undofile
+en
+" }}}
+
+fu! MarkMargin (on)
+  if exists('b:MarkMargin')
+    try
+      cal matchdelete(b:MarkMargin)
+    cat /./
+    endt
+    unlet b:MarkMargin
+  en
+  if a:on
+    let b:MarkMargin = matchadd('ColorColumn', '\%81v\s*\zs\S', 100)
+  en
+endf
+
+aug MarkMargin
+  au!
+  au BufEnter * :cal MarkMargin(1)
+  au BufEnter *.vp* :cal MarkMargin(0)
+aug END
+]])
+
+vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
