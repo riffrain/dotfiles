816dd565f2bf212ed6a3e015c21897efde8fb640
riffrain <raindropops@gmail.com>
2022-02-02 22:50:09 +0900

    update

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
 ino jj <Esc>
-nn <silent><Esc><Esc> :<C-u>se nohlsearch!<CR>
+nn <C-l> :<C-u>se nohlsearch!<CR>
 no j gj
 no k gk
 no gj j
@@ -13,4 +13,12 @@ nn <Leader>s :<C-u>sp<CR>
 nn <Leader>v :<C-u>vs<CR>
 nn <Leader>T :<C-u>tabnew<cr>
 no <Leader>n :<C-u>:setl number!<CR>
+
+nn x "_x
+nn s "_s
+
+nn <Leader>t <Cmd>terminal<CR>
+tno <silent><Esc><Esc> <C-\><C-n>
+au TermOpen * startinsert
+au WinEnter * if &buftype ==# 'terminal' | startinsert | endif
 ]])
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -89,7 +89,7 @@ require('packer').startup(function()
     config = function ()
       vim.cmd([[
         call ddc#custom#patch_global('sources', ['nvim-lsp', 'around', 'buffer', 'file'])
-        " call ddc#custom#patch_global('completionMenu', 'pum.vim')
+        call ddc#custom#patch_global('completionMenu', 'pum.vim')
         call ddc#custom#patch_global('sourceOptions', {
           \ '_': {
           \   'matchers': ['matcher_onp'],
@@ -122,11 +122,12 @@ require('packer').startup(function()
         "       \ pum#visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' :
         "       \ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
         "       \ '<TAB>' : ddc#manual_complete()
-        " inoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
-        " inoremap <C-n>   <Cmd>call pum#map#select_relative(+1)<CR>
-        " inoremap <C-p>   <Cmd>call pum#map#select_relative(-1)<CR>
-        " inoremap <C-y>   <Cmd>call pum#map#confirm()<CR>
-        " inoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
+        inoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
+        inoremap <C-n>   <Cmd>call pum#map#select_relative(+1)<CR>
+        inoremap <C-p>   <Cmd>call pum#map#select_relative(-1)<CR>
+        inoremap <C-y>   <Cmd>call pum#map#confirm()<CR>
+        inoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
+        inoremap <silent><expr> <TAB> pum#visible() ? '<Cmd>call pum#map#confirm()<CR>' : '<TAB>'
       ]])
     end
   }
