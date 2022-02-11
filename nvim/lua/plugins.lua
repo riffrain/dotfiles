67faadc9efa2c286c94739e86a57358163166799
riffrain <raindropops@gmail.com>
2022-02-11 10:54:49 +0900

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -167,22 +167,32 @@ require('packer').startup(function()
             return vim_item
           end
         },
-        mapping = {
-          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-          ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` i you want to remove the default `<C-y>` mapping.
-          ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-          }),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        },
+        -- mapping = {
+        --   ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        --   ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        --   ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        --   ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` i you want to remove the default `<C-y>` mapping.
+        --   ['<C-e>'] = cmp.mapping({
+        --     i = cmp.mapping.abort(),
+        --     c = cmp.mapping.close(),
+        --   }),
+        --   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- },
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'vsnip' },
-        }, {
-          { name = 'buffer' },
+          { name = 'buffer',
+            opts = {
+              get_bufnrs = function()
+                local bufs = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                  bufs[vim.api.nvim_win_get_buf(win)] = true
+                end
+
+                return vim.tbl_keys(bufs)
+              end
+            }
+          },
         })
       })
       -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
