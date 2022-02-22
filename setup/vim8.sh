461eb721c86cca3d4ca72108c98e7e9c25a98a58
riffrain <raindropops@gmail.com>
2022-02-22 21:36:20 +0900

    change vimrc path

--- setup/vim8.sh
+++ setup/vim8.sh
@@ -2,10 +2,6 @@
 
 path=$(cd $(dirname $0); pwd)
 
-if [ -f ~/.vimrc ]; then
-  echo 'move exists .vimrc (~/.vimrc -> ~/.vimrc.original)'
-  mv ~/.vimrc ~/.vimrc.orig
-fi
 ln -s $path'/../.vimrc' ~/.vimrc
 if [ -d "~/.vim" ]; then
   echo 'move exists .vim (~/.vim -> ~/.vim.original)'
