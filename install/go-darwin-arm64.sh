f8a8710ad9d30ab3d1326d21e9b37a37c457b4b5
riffrain <raindropops@gmail.com>
2022-05-05 20:52:42 +0900

    fix

--- install/go-darwin-arm64.sh
+++ install/go-darwin-arm64.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -f ~/.go ]; then
+if [ -d ~/.go ]; then
   echo "already exists: ~/.go"
-  exist 1
+  exit 1
 fi
 
 path=$(cd $(dirname $0); pwd)
@@ -14,5 +14,7 @@ curl -LO https://go.dev/dl/go1.18.1.darwin-arm64.tar.gz
 tar -zxvf ./go1.18.1.darwin-arm64.tar.gz
 
 mv ./go ~/.go
+cd $path
+rm -r tmp
 
 echo 'installed golang to ~/.go'
