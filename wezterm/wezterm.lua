8821f33626b89a18d69130da310f84ea5b85029c
riffrain <raindropops@gmail.com>
2022-10-30 16:05:31 +0900

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,46 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font_with_fallback {
+    'FiraCode Nerd Font',
+    'Migu 1M',
+  },
+  font_size = 12.0,
+  use_ime = true,
+  tab_bar_at_bottom = false,
+  color_scheme = "Molokai (Gogh)",
+  enable_scroll_bar = true,
+  window_padding = {
+    left = 5,
+    right = 5,
+    top = 0,
+    bottom = 0,
+  },
+  keys = {
+    {
+      key = 'd',
+      mods = 'CMD',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'D',
+      mods = 'CMD',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'l',
+      mods = 'SHIFT',
+      action = wezterm.action.Nop,
+    },
+    {
+      key = 'Enter',
+      mods = 'CMD',
+      action = wezterm.action.ToggleFullScreen,
+    },
+    {
+      key = '¥',
+      mods = 'CMD',
+      action = wezterm.action.PaneSelect,
+    },
+  },
+}
